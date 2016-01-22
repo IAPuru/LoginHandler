bb4745bf30f556a50d2a12634ff5afd046c3fbd0
Purushottam Dangol <puruep@instantapi.co>
2016-01-22 09:47:37 +0545

    nav bar checkin

---
+++ applicationjs/flexsecure.js
@@ -0,0 +1,63 @@
+﻿function SecureLogin (email) {
+    this.email = email;
+    console.log('SecureLogin instantiated');
+
+    this.UserAuthenticate = function () {
+        console.log('UserAuthenticate called');
+        
+    };
+
+    this.SystemAuthenticate = function () {
+        return true;
+    };
+
+    this.SystemPoll = function () {
+        return true;
+    };
+}
+
+
+function ValidateLogin()
+{
+    var email = $("#txtEmail").val();
+    var user = new SecureLogin(email);
+    user.UserAuthenticate();
+   
+    if ($("#btnLogin").text() == "Next") {
+        $("#divPin").show();
+        $("#divEmail").hide();
+        $("#btnLogin").text("Login");
+    }
+    else {
+        HideControls();
+        $("#status").html("Processing please wait..");
+       
+        setTimeout(LoginSuccess, 2000);
+    }
+   
+}
+
+function HideControls()
+{
+    $("#divPin").hide();
+    $("#divEmail").hide();
+    $("#btnLogin").hide();
+}
+
+function ShowControls() {
+   
+    $("#divEmail").show();
+    $("#btnLogin").text("Next");
+    $("#btnLogin").show();
+    $("#status").html("");
+}
+
+function LoginSuccess() {
+    $("#status").html("Login success.");
+    setTimeout(LoginOut, 2000);
+}
+
+function LoginOut() {
+    $("#status").html("Logout...");
+    setTimeout(ShowControls, 1000);
+}
