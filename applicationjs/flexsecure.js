2c4e21ea81e19dc05d536e9046585e8a8bbe1be5
Purushottam Dangol <puruep@instantapi.co>
2016-01-27 09:18:19 +0545

    Modified as suggested..

--- applicationjs/flexsecure.js
+++ applicationjs/flexsecure.js
@@ -1,73 +1,52 @@
-﻿function SecureLogin (email) {
-    this.email = email;
-    console.log('SecureLogin instantiated');
-
-    this.UserAuthenticate = function () {
+﻿function flexsecure(key) {
+   
+    this.apikey = key;
+    console.log('Flexsecure sdk instantiated');
+    
+    this.userAuthenticate = function (email, system) {
+      
         console.log('UserAuthenticate called');
-        return true;
-        
+        //result = this.helper.restPost("/user/authenticate", { "email": email, "system": system } )
+
+        // raise event for result
+
+        return true;        
     };
 
-    this.SystemAuthenticate = function () {
+    this.systemAuthenticate = function (email, system, mode) {
         console.log('SystemAuthenticate called');
         return true;
     };
 
-    this.SystemPoll = function () {
+    this.systemPoll = function () {
+        //setup timer for 10 sec
+        // raise event Passwordless
+        // polling every 1 second,
+        // - restPost URL= "/system/poll", body email, system
+        // - 1. 60% false, - no response yet
+        // - 2. 10% true - "Refused"
+        // - 3. 30% true - "Approved"
+        // - 4. if no true result by end of 10 sec timer, retrun false
+        //  -- stop timer, rasie event timeout..
+
+        // stop timer
+        // rasie event on true condition  
+        
         return true;
     };
-}
 
+  //  this.helper.restPost (URL, BODY) {
+        // retrn result of a post call using this.key in header
+        // header: apikey: this.key, Content-Type: application/json
 
-function ValidateLogin()
-{
-    var email = $("#txtEmail").val();
-    var user = new SecureLogin(email);
-    if (user.UserAuthenticate()) {
-        if ($("#btnLogin").text() == "Next") {
-            $("#divPin").show();
-            $("#divEmail").hide();
-            $("#btnLogin").text("Login");
-        }
-        else {
-            $("#status").html("Processing please wait..");
-            if (user.SystemAuthenticate()) {
-                HideControls();
-                setTimeout(LoginSuccess, 2000);
-            }
-            else
-                LoginFailed();//default login page
-        }
+        // swicth/if logic agains URL
+
+        // if URL is /user/authenticate fake response
+        // - A (50%).  { "status": 200, "data": { "mode": "PIN" } }
+        // - B (50%).  { "status": 200, "data": { "mode": "PASSWORDLESS" } }
+        
     }
-   
-}
 
-function HideControls()
-{
-    $("#divPin").hide();
-    $("#divEmail").hide();
-    $("#btnLogin").hide();
-}
 
-function ShowControls() {
-   
-    $("#divEmail").show();
-    $("#btnLogin").text("Next");
-    $("#btnLogin").show();
-    $("#status").html("");
-}
 
-function LoginSuccess() {
-    $("#status").html("Login success.");
-    setTimeout(LoginOut, 2000);
-}
 
-function LoginFailed() {
-    $("#status").html("Login failed.");
-    window.location.href = 'index.html';
-}
-
-function LoginOut() {
-    $("#status").html("Logout...");
-    setTimeout(ShowControls, 1000);
-}
---
+++ applicationjs/main.js
@@ -0,0 +1,54 @@
+﻿
+function ValidateLogin() {
+    var email = $("#txtEmail").val();
+    var apikey = '1234';
+    
+    var flexapi = new flexsecure(apikey);
+
+    if (flexapi.userAuthenticate(email, "pin")) {
+        if ($("#btnLogin").text() == "Next") {
+            $("#divPin").show();
+            $("#divEmail").hide();
+            $("#btnLogin").text("Login");
+        }
+        else {
+            $("#status").html("Processing please wait..");
+            if (flexapi.systemAuthenticate()) {
+                HideControls();
+                setTimeout(LoginSuccess, 2000);
+            }
+            else
+                LoginFailed();//default login page
+        }
+    }
+
+}
+
+function HideControls() {
+    $("#divPin").hide();
+    $("#divEmail").hide();
+    $("#btnLogin").hide();
+}
+
+function ShowControls() {
+
+    $("#divEmail").show();
+    $("#btnLogin").text("Next");
+    $("#btnLogin").show();
+    $("#status").html("");
+}
+
+function LoginSuccess() {
+    $("#status").html("Login success.");
+    setTimeout(LoginOut, 2000);
+}
+
+function LoginFailed() {
+    $("#status").html("Login failed.");
+    window.location.href = 'index.html';
+}
+
+function LoginOut() {
+    $("#status").html("Logout...");
+    setTimeout(ShowControls, 1000);
+}
